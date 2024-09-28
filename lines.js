a52c9fe1a9f50653b0db02ad18849aedaa2d94d6
DimaTurkey <123176664+DimaTurkey@users.noreply.github.com>
2024-09-28 18:42:04 +0300

    dfghjkl

---
+++ lines.js
@@ -0,0 +1,19 @@
+let math = 5 + 5 + '5';
+console.log(math);
+
+const email = 'theturkeyprogramist12@gmail.com'; 
+const hasSpam = email.includes("@");
+const haspam = email.includes("had");
+console.log(hasSpam);
+console.log(haspam);
+console.log('кількість символів в email: ' + email.length);
+
+let word1 = 'my';
+let word2 = 'name';
+let word3 = 'is';
+let fullname = word1 + ' ' + word2 + ' ' + word3 + ' Viktorian';
+console.log(fullname);
+
+let name = 'Олександр'; 
+let pay = 30; 
+alert('Дякуємо, ' + name + ' До сплати ' + pay + ' гривень.');
